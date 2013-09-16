9fef40a7adedb51c26250acd81e360e3da130ff5
Ross Williamson <rwilliamson@astro.caltech.edu>
2013-09-16 11:20:07 -0700

    Screwed up adc5g_init - now should be ok

--- startup.m
+++ startup.m
@@ -4,13 +4,11 @@ warning off Simulink:Engine:SaveWithParameterizedLinks_Warning
 warning off Simulink:Engine:SaveWithDisabledLinks_Warning
 warning off Simulink:Commands:LoadMdlParameterizedLink
 
-if length(getenv('XILINX_PATH')) == 0
-  setenv('XILINX_PATH', regexprep(getenv('XILINX'),'/ISE$',''));
-end
 addpath([getenv('XILINX_PATH'), '/ISE/sysgen/util/']);
 addpath([getenv('XILINX_PATH'), '/ISE/sysgen/bin/lin64']);
 addpath([getenv('MLIB_DEVEL_PATH'), '/casper_library']);
 addpath([getenv('MLIB_DEVEL_PATH'), '/xps_library']);
+addpath('/home/rw247/monroe_library/casper_libraries/mlib_devel/gavrt_library')
 addpath('/home/rw247/monroe_library/casper_libraries/xblocks_devel/xblocks_library')
 addpath('/home/rw247/monroe_library/monroe_library/xblock_helper_functions')
 addpath('/home/rw247/monroe_library/monroe_library')
@@ -18,4 +16,3 @@ xlAddSysgen([getenv('XILINX_PATH'), '/ISE'])
 sysgen_startup
 load_system('casper_library');
 load_system('xps_library');
-load_system('monroe_library');
